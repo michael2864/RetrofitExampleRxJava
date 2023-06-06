0f6eea13217e68afcb537aca79f36d72a7fd62f7
Mike Shevchenko <*>
2023-06-06 13:53:14 +0200

    Change Search bar logic Rx Computation thread switch in debounce method Serach is now working

--- app/src/main/java/ru/mikhailskiy/retrofitexample/MainActivity.kt
+++ app/src/main/java/ru/mikhailskiy/retrofitexample/MainActivity.kt
@@ -3,21 +3,14 @@ package ru.mikhailskiy.retrofitexample
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
-
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.android.synthetic.main.activity_main.*
 import ru.mikhailskiy.retrofitexample.adapters.MoviesAdapter
-import ru.mikhailskiy.retrofitexample.data.MoviesResponse
+import ru.mikhailskiy.retrofitexample.data.Movie
 import ru.mikhailskiy.retrofitexample.network.MovieApiClient
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,27 +19,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // add simple recyclerview
-        val recyclerView = findViewById<RecyclerView>(R.id.movies_recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        search_toolbar
+            .onTextChangedObservable
+            .map { it.trim() }
+            .doOnNext { Log.d("THR# 38", Thread.currentThread().name) }
+            .debounce(500, TimeUnit.MILLISECONDS)
+            .doOnNext { Log.d("THR# 40", Thread.currentThread().name) }
+            .filter { it.isNotEmpty() }
+            .doOnNext { Log.d("THR# 42", Thread.currentThread().name) }
+            .observeOn(Schedulers.io())
+            .doOnNext { Log.d("THR# 44", Thread.currentThread().name) }
+            .flatMapSingle { it -> MovieApiClient.apiClient.searchByQuery(MainActivity.API_KEY, "ru", it) }
+            .doOnNext { Log.d("THR# After flatMap ", Thread.currentThread().name) }
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnNext { Log.d("THR# after AnSchedulers", Thread.currentThread().name) }
+            .subscribe({
+                setMovies(it.results)
+                Log.d(MainActivity.TAG, it.toString())
+            }, {
+                Log.e(MainActivity.TAG, it.toString())
+            })
 
-        // receive single instead call
+
+        // Получаем Single
         val getTopRatedMovies = MovieApiClient.apiClient.getTopRatedMovies(API_KEY, "ru")
 
         getTopRatedMovies
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
-                { it ->
+                {
                     val movies = it.results
-                    // to recyclreview
-                    recyclerView.adapter = MoviesAdapter(movies, R.layout.list_item_movie)
+                    // Передаем результат в adapter и отображаем элементы
+                    movies_recycler_view.adapter = MoviesAdapter(movies, R.layout.list_item_movie)
                 },
                 { error ->
-                    // error
+                    // Логируем ошибку
                     Log.e(TAG, error.toString())
                 }
             )
+    }
+
+    fun setMovies(movies: List<Movie>) {
+        movies_recycler_view.adapter = MoviesAdapter(movies, R.layout.list_item_movie)
     }
 
     companion object {
@@ -54,8 +69,6 @@ class MainActivity : AppCompatActivity() {
         private val TAG = MainActivity::class.java.simpleName
 
         // TODO - insert your themoviedb.org API KEY here
-        private val API_KEY = ""
-        // private val API_KEY = "7e8f60e325cd06e164799af1e317d7a7"  for example
+        private val API_KEY = ""  //for example h6786a78c721f657a78e0cb1d75ab346
     }
 }
-
--- app/src/main/java/ru/mikhailskiy/retrofitexample/network/MovieApiInterface.kt
+++ app/src/main/java/ru/mikhailskiy/retrofitexample/network/MovieApiInterface.kt
@@ -9,8 +9,8 @@ import ru.mikhailskiy.retrofitexample.data.MoviesResponse
 interface MovieApiInterface {
 
     @GET("movie/top_rated")
-    fun getTopRatedMovies(
-        @Query("api_key") apiKey: String,
-        @Query("language") language: String):
-            Single<MoviesResponse>     //replace call interface to single
+    fun getTopRatedMovies(@Query("api_key") apiKey: String, @Query("language") language: String): Single<MoviesResponse>
+
+    @GET("search/movie")
+    fun searchByQuery(@Query("api_key") apiKey: String, @Query("language") language: String, @Query("query") query: String): Single<MoviesResponse>
 }
